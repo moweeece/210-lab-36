108ec50124d5be7d7d7a45eae380249e8ca74675
moweeece <mjespinosa990@gmail.com>
2024-11-27 12:16:36 -0800

    Update 2

--- main.cpp
+++ main.cpp
@@ -7,22 +7,64 @@ using namespace std;
 int main() {
 
     // BST
+    StringBinaryTree tree;
+
     // read from file (strings)
+    ifstream file("codes.txt");
+    string code;
+
+    // if the file exists is true
+    if(file)
+    {
+        // loop to iterate through all lines of code
+        while (getline(file, code))
+        {
+            // add the code to the tree
+            tree.insertNode(code);
+        }
+        // close the file when BST population is completed
+        file.close();
+    }
+    else  // error opening the file
+    {
+        cout << "Error opening the file.\n";
+        return 1;
+    }
     
     while(true)
+    {
+        int userChoice;
 
+        cout << "BST Menu:\n";
+        cout << "=========\n";
+        cout << "[1] Add a record\n";
+        cout << "[2] Delete a record\n";
+        cout << "[3] Search for a record\n";
+        cout << "[4] Modify a record\n";
+        cout << "[5] Exit\n";
+        cin >> userChoice;
 
-    // add
+        switch(userChoice)
+        {
+            case 1:
+                // add
+                break;
+            case 2:
+                // delete
+            case 3:
+                // search
+            case 4:
+                // modify
+            case 5:
+                cout << "Exiting the program.\n";
+                return 0;
+                // exit
+            default:
+                // default - invalid choice
+                cout << "Invalid choice. Enter a number between 1 and 5.\n";
+        }
+    }
 
-    // delete
-
-    // search
-
-    // modify
-
-    // exit
-
-    // default - invalid choice
 
     return 0;
 }
