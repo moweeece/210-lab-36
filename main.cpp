ccd004be84d1e2b5305c0ef6fad0cda9b5180c94
moweeece <mjespinosa990@gmail.com>
2024-11-27 12:06:11 -0800

    Update 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#include "IntBinaryTree.h"
+#include <string>
+#include <fstream>
+#include "StringBinaryTree.h"
 using namespace std;
 
 int main() {
+
+    // BST
+    // read from file (strings)
     
+    while(true)
+
+
+    // add
+
+    // delete
+
+    // search
+
+    // modify
+
+    // exit
+
+    // default - invalid choice
 
     return 0;
 }
